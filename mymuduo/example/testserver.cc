bb9569b8541c5cf77c091c8af404b5c35c6e1138
TangPei99 <1191738896@qq.com>
2021-12-29 16:49:41 +0800

    muduo

---
+++ mymuduo/example/testserver.cc
@@ -0,0 +1,69 @@
+#include <mymuduo/TcpServer.h>
+#include <mymuduo/Logger.h>
+
+#include <string>
+#include <functional>
+
+class EchoServer
+{
+public:
+    EchoServer(EventLoop *loop,
+            const InetAddress &addr, 
+            const std::string &name)
+        : server_(loop, addr, name)
+        , loop_(loop)
+    {
+        // 注册回调函数
+        server_.setConnectionCallback(
+            std::bind(&EchoServer::onConnection, this, std::placeholders::_1)
+        );
+
+        server_.setMessageCallback(
+            std::bind(&EchoServer::onMessage, this,
+                std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)
+        );
+
+        server_.setThreadNum(8);
+    }
+    void start()
+    {
+        server_.start();
+    }
+private:
+    // 连接建立或者断开的回调
+    void onConnection(const TcpConnectionPtr &conn)
+    {
+        if (conn->connected())
+        {
+            LOG_INFO("Connection UP : %s", conn->peerAddress().toIpPort().c_str());
+        }
+        else
+        {
+            LOG_INFO("Connection DOWN : %s", conn->peerAddress().toIpPort().c_str());
+        }
+    }
+
+    //读事件回调
+    void onMessage(const TcpConnectionPtr &conn,
+                Buffer *buf,
+                Timestamp time)
+    {
+        std::string msg = buf->retrieveAllAsString();
+        conn->send(msg);
+        conn->shutdown(); 
+    }
+
+    EventLoop *loop_;
+    TcpServer server_;
+};
+
+int main()
+{
+    EventLoop loop;
+    InetAddress addr(5000);
+    EchoServer server(&loop, addr, "EchoServer"); 
+    server.start(); 
+    loop.loop(); 
+
+    return 0;
+}
